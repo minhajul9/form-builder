b555076b3a3da9b2a84a2a992ebce060ebb4e76d
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-07-25 17:56:45 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { FaPlus } from 'react-icons/fa';
+
+
+function App() {
+
+  return (
+    <div className='md:w-4/5 mx-auto'>
+      <div className="navbar bg-slate-800 text-white">
+        <a className="btn btn-ghost normal-case text-xl">Form Builder</a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
