461a6785826a3367ecd1cbaa610a658778ccbb8b
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-07-26 10:34:53 +0600

    Create form title

--- src/Components/CreateNewForm.jsx
+++ src/Components/CreateNewForm.jsx
@@ -1,8 +1,53 @@
+import { FaPlus } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import { useLocation } from 'react-router-dom';
+
 
 const CreateNewForm = () => {
+
+    const location = useLocation();
+    // console.log(location.state.name);
+
+    const formObject = {
+        name: location.state.name,
+        fields: []
+    }
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const addField = (data) => {
+        console.log(data);
+    }
+
     return (
         <div>
-            create new form
+            <div className='md:w-2/3 mx-auto'>
+                <h1 className='text-3xl font-bold text-center'>{formObject.name}</h1>
+        
+                {/* Open the modal using ID.showModal() method */}
+                <button className="btn" onClick={() => window.my_modal_2.showModal()}><FaPlus></FaPlus> Add Field</button>
+                <dialog id="my_modal_2" className="modal">
+                    <form onSubmit={handleSubmit(addField)} method="dialog" className="modal-box">
+                        <h3 className="font-bold text-lg">Enter field information.</h3><br />
+
+                        <input className='m-2 p-2 rounded w-full' type="text" placeholder='Name' {...register('name', {required: true})} /> <br />
+
+                        <input className='m-2 p-2 rounded w-full' type="text" placeholder='Placeholder' {...register('placeholder', {required: true})} /> <br />
+
+                        <input className='m-2 p-2 rounded w-full' type="type" placeholder='Type of data' {...register('type', {required: true})} /> <br />
+
+                        <input className='btn btn-success m-2' type="submit" value="Add" />
+                        
+                    </form>
+                    <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                    </form>
+                </dialog>
+            </div>
         </div>
     );
 };
--- src/Components/FormList.jsx
+++ src/Components/FormList.jsx
@@ -1,17 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa';
+import Swal from 'sweetalert2'
+
 
 
 const FormList = () => {
+    const navigate = useNavigate();
+
+    const createForm = () => {
+        Swal.fire({
+            title: 'Enter Form Name',
+            input: 'text',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Create',
+            showLoaderOnConfirm: true,
+            preConfirm: (login) => {
+                
+                if (login) {
+                    navigate('/createForm', { state: { name: login} })
+                }
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        })
+    }
+
     return (
         <div className='mt-20'>
 
-            <Link to='/createForm' className='flex justify-center h-56 w-56 rounded-md text-2xl border'>
-                <div className='flex flex-col justify-center items-center'>
+            <button onClick={createForm}>
+                <div className='flex flex-col justify-center items-center h-56 w-56 rounded-md gap-6 text-2xl border'>
                     <FaPlus></FaPlus>
                     <span>Create New Form</span>
                 </div>
-            </Link>
+            </button>
         </div>
     );
 };
