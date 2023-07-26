7173fbed945ae1bc319ba6fa9aa98e2b4f848cee
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-07-26 12:10:01 +0600

    field delete button added

--- src/Components/CreateNewForm.jsx
+++ src/Components/CreateNewForm.jsx
@@ -1,4 +1,4 @@
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaTrashAlt } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
@@ -26,7 +26,13 @@ const CreateNewForm = () => {
         reset();
         document.getElementById('close').click();
     }
-    console.log(formObject.fields);
+
+    const removeField = name => {
+        const newFields = formObject.fields.filter(field => field.name !== name)
+        const newForm = {name: formObject.name, fields: newFields}
+        setFormObject(newForm)
+    }
+
     return (
         <div>
             <div className='md:w-2/3 mx-auto'>
@@ -35,13 +41,17 @@ const CreateNewForm = () => {
                 <div className='md:w-1/2 mx-auto border mt-8 p-6 rounded-lg flex flex-col items-center'>
                     {
                         formObject.fields.map(field =>
-                            <input
-                                key={field.name}
-                                className='m-2 p-2 rounded w-full'
-                                type={field.type}
-                                placeholder={field.placeholder}
-                                name={field.name}
-                            />
+                            <div key={field.name} className='flex items-center justify-around w-full'>
+                                <input
+                                    className='m-2 p-2 rounded w-full'
+                                    type={field.type}
+                                    placeholder={field.placeholder}
+                                    name={field.name}
+                                />
+                                <button onClick={() => removeField(field.name)}>
+                                    <FaTrashAlt className='text-red-700'></FaTrashAlt>
+                                </button>
+                            </div>
                         )
                     }
 
@@ -53,22 +63,22 @@ const CreateNewForm = () => {
                 <dialog id="my_modal_2" className="modal">
                     <form onSubmit={handleSubmit(addField)} method="dialog" className="modal-box">
                         <h3 className="font-bold text-lg">Enter field information.</h3><br />
-                        
+
 
                         <input className='m-2 p-2 rounded w-full' type="text" placeholder='Name' {...register('name', { required: true })} /> <br />
-                        <p className='text-red-700 ms-4'>{errors.name? "This field is required" : ''}</p>
+                        <p className='text-red-700 ms-4'>{errors.name ? "This field is required" : ''}</p>
 
                         <input className='m-2 p-2 rounded w-full' type="text" placeholder='Placeholder' {...register('placeholder', { required: true })} /> <br />
-                        <p className='text-red-700 ms-4'>{errors.placeholder? "This field is required" : ''}</p>
+                        <p className='text-red-700 ms-4'>{errors.placeholder ? "This field is required" : ''}</p>
 
-                        
+
                         <select placeholder='Type of data' {...register('type', { required: true })} className='m-2 p-2 rounded w-full' >
                             <option value=""></option>
                             <option value="text">Text</option>
                             <option value="email">Email</option>
                             <option value="number">Number</option>
                         </select> <br />
-                        <p className='text-red-700 ms-4'>{errors.type? "This field is required" : ''}</p>
+                        <p className='text-red-700 ms-4'>{errors.type ? "This field is required" : ''}</p>
 
                         <input className='btn btn-success m-2' type="submit" value="Add" />
 
