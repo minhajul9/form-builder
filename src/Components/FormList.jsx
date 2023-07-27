cb2adc71f81e372d5f04726efb0a8f9e485bec3e
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-07-28 00:20:08 +0600

    form list data load from database

--- src/Components/FormList.jsx
+++ src/Components/FormList.jsx
@@ -1,11 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa';
 import Swal from 'sweetalert2'
+import { useEffect, useState } from 'react';
 
 
 
 const FormList = () => {
     const navigate = useNavigate();
+
+    const [forms, setForms] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/forms')
+            .then(res => res.json())
+            .then(data => setForms(data))
+    }, [])
+
+    // console.log(forms);
 
     const createForm = () => {
         Swal.fire({
@@ -18,9 +29,9 @@ const FormList = () => {
             confirmButtonText: 'Create',
             showLoaderOnConfirm: true,
             preConfirm: (login) => {
-                
+
                 if (login) {
-                    navigate('/createForm', { state: { name: login} })
+                    navigate('/createForm', { state: { name: login } })
                 }
             },
             allowOutsideClick: () => !Swal.isLoading()
@@ -28,7 +39,17 @@ const FormList = () => {
     }
 
     return (
-        <div className='mt-20'>
+        <div className='mt-20 grid grid-cols-2 md:grid-cols-4'>
+
+            {
+                forms.map(form =>
+                    <button key={form._id} >
+                        <div className='flex flex-col justify-center items-center h-56 w-56 rounded-md gap-6 text-2xl border bg-slate-700 text-white'>
+                            <span>{form.name}</span>
+                        </div>
+                    </button>
+                )
+            }
 
             <button onClick={createForm}>
                 <div className='flex flex-col justify-center items-center h-56 w-56 rounded-md gap-6 text-2xl border'>
